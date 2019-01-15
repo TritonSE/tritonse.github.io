e20e7744cef387b1e8c68eb4e333a53aa5ad9688
David Hacker <dmhacker@protonmail.com>
2019-01-14 18:47:08 -0800

    Re-add tritonse-source-firebase plugin; remove submodule status

---
+++ plugins/tritonse-source-firebase/gatsby-node.js
@@ -0,0 +1,54 @@
+const firebase = require("firebase")
+
+exports.sourceNodes = (
+  { actions, createNodeId, createContentDigest },
+  { types },
+  done
+) => {
+  const { createNode } = actions 
+
+  firebase.initializeApp({
+    apiKey: "AIzaSyBtdsy5_rUnaKB_A4f_SFMulkKqC4loxFg",
+    authDomain: "tritonse-web.firebaseapp.com",
+    databaseURL: "https://tritonse-web.firebaseio.com",
+    projectId: "tritonse-web",
+    storageBucket: "tritonse-web.appspot.com",
+    messagingSenderId: "370639204520"
+  })
+
+  const db = firebase.firestore()
+
+  db.settings({
+    timestampsInSnapshots: true
+  });
+
+  const start = Date.now()
+
+  types.forEach(
+    ({ query = ref => ref, map = node => node, type, path }) => {
+      console.log(`\n[Firebase] Fetching data for ${type}...`)
+
+      db.collection(path).get().then(snapshot => {
+        console.log(`\n[Firebase] Data for ${type} loaded in ${Date.now() - start} ms.`)
+
+        snapshot.forEach(doc => {
+          const node = Object.assign({}, doc.data());
+
+          createNode(
+            Object.assign(node, {
+              id: doc.id,
+              parent: null,
+              children: [],
+              internal: {
+                type: `FirebaseDocument`,
+                content: JSON.stringify(doc.data()),
+                contentDigest: createContentDigest(node) 
+              }
+            })
+          )
+        });
+
+        done()
+      })
+    });
+}
