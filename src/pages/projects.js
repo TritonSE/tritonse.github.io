5cfcd6fc506d2ee6677e3ccfe37cc0822d61ea60
David Hacker <dmhacker@protonmail.com>
2019-01-15 18:46:39 -0800

    Add empty projects file

---
+++ src/pages/projects.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
+
+import Navbar from '../components/navbar.js'
+import Footer from '../components/footer.js'
+
+import '../../node_modules/siimple/dist/siimple.min.css'
+import '../styles/site.css'
+
+export default ({data}) => {
+  return (<div>
+    <Navbar></Navbar> 
+    <div class="siimple-jumbotron tse-jumbotron-normal siimple-jumbotron--large">
+      <div class="siimple-jumbotron-title">Ongoing & Past Projects</div>
+      <div class="siimple-jumbotron-subtitle">Take a look at some of our work with non-profits</div>
+    </div>
+    <Footer></Footer> 
+  </div>)
+}
