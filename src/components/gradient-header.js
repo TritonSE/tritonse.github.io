f7194d45d817865b4353aafb710675214b08c605
David Hacker <dmhacker@protonmail.com>
2019-09-08 11:40:46 -0400

    Enable waves manually in componentDidMount()

--- src/components/gradient-header.js
+++ src/components/gradient-header.js
@@ -8,53 +8,53 @@ import '../../node_modules/materialize-css/dist/css/materialize.min.css'
 
 class Header extends React.Component {
   componentDidMount() {
-    require('../../node_modules/materialize-css/dist/js/materialize.min.js')
+    require('../../node_modules/materialize-css/dist/js/materialize.min.js');
     window.M.AutoInit();
+    window.Waves.displayEffect();
   }
 
   render() {
     const {data, title, subtitle, buttons, background} = this.props;
-      return (
-<div>
-    <ul id="tse-navbar-dropdown" className="dropdown-content">
-      <li><Link to="/students">Students</Link></li>
-      <li><Link to="/nonprofits">Nonprofits</Link></li>
-    </ul>
-    <nav className="blue darken-3">
-      <div className="nav-wrapper container">
-        <div className="row">
-          <div className="col l6 s12">
-            <Link to="/">
-              <Img fluid={data.logo.childImageSharp.fluid} className="tse-navbar-logo" ></Img>
-            </Link>
-          </div>
-          <div className="col l6 s12 hide-on-med-and-down">
-            <ul className="right hide-on-med-and-down">
-              <li><Link to="/about">About</Link></li>
-              <li><Link to="/projects">Projects</Link></li>
-              <li><a className="dropdown-trigger" href="#!" data-target="tse-navbar-dropdown">Contact</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </nav>
-    <div className={`tse-header ${background ? background : 'tse-header-gradient'}`}>
-      <div className="container">
-        <div className="row center white-text">
-          <div className="col s12">
-            <h1>{title}</h1>
-          </div>
-          <div className="col s12">
-            <h5>{subtitle}</h5>
-          </div>
-          <div className="col s12 tse-header-buttons">
-            {buttons}
+    return (<div>
+      <ul id="tse-navbar-dropdown" className="dropdown-content">
+        <li><Link to="/students">Students</Link></li>
+        <li><Link to="/nonprofits">Nonprofits</Link></li>
+      </ul>
+      <nav className="blue darken-3">
+        <div className="nav-wrapper container">
+          <div className="row">
+            <div className="col l6 s12">
+              <Link to="/">
+                <Img fluid={data.logo.childImageSharp.fluid} className="tse-navbar-logo" ></Img>
+              </Link>
+            </div>
+            <div className="col l6 s12 hide-on-med-and-down">
+              <ul className="right hide-on-med-and-down">
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/projects">Projects</Link></li>
+                <li><a className="dropdown-trigger" href="#!" data-target="tse-navbar-dropdown">Contact</a></li>
+              </ul>
+            </div>
           </div>
         </div>
-        <p></p>
+      </nav>
+      <div className={`tse-header ${background ? background : 'tse-header-gradient'}`}>
+        <div className="container">
+          <div className="row center white-text">
+            <div className="col s12">
+              <h1>{title}</h1>
+            </div>
+            <div className="col s12">
+              <h5>{subtitle}</h5>
+            </div>
+            <div className="col s12 tse-header-buttons">
+              {buttons}
+            </div>
+          </div>
+          <p></p>
+        </div>
       </div>
-    </div>
-  </div>);
+    </div>);
   }
 }
 
