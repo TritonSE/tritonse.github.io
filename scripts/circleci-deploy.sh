d09650e7f4c9c04b1d5c220d3d0fcb46999b48ab
David Hacker <dmhacker@protonmail.com>
2020-01-17 13:23:43 -0800

    Install sshpass in CircleCI config as separate command

--- scripts/circleci-deploy.sh
+++ scripts/circleci-deploy.sh
@@ -11,8 +11,5 @@ rm -rf ../public
 git add .
 git commit -m "[ci skip] Deploy build files to master branch"
 git push
-apt-get install sshpass
- sshpass -p $ACSWEB_PASSWORD ssh tse@acsweb.ucsd.edu
-./refresh.sh
-exit
-history -r
+sshpass -e ssh tse@acsweb.ucsd.edu
+sh refresh.sh
