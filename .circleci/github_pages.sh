c627ede702d832ed1e16af0060fc02e747a0c70c
David Hacker <dmhacker@protonmail.com>
2021-01-06 16:35:49 -0800

    Fix issue with CircleCI overwriting main branch

--- .circleci/github_pages.sh
+++ .circleci/github_pages.sh
@@ -4,10 +4,12 @@ git config --global user.email "tse@ucsd.edu"
 git config --global user.name "TSE CircleCI Bot"
 git pull
 mv public ..
+git stash
+git stash drop
 git checkout gh-pages
 rm -rf *
 mv ../public/* .
 rm -rf ../public
 git add .
-git commit -m "[ci skip] Deploy build files to master branch"
+git commit -m "[ci skip] Deploying build files to GitHub Pages"
 git push
