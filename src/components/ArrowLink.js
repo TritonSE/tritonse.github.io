627658d3910b7f61c6b57defa6a0037f8fa787dd
Justin Yao Du <justinyaodu@gmail.com>
2021-06-30 19:15:15 -0700

    Improve ArrowLink styling (#57)

--- src/components/ArrowLink.js
+++ src/components/ArrowLink.js
@@ -2,14 +2,23 @@ import React from "react";
 import { Link } from "gatsby";
 import { BsArrowRight } from "react-icons/bs";
 
+import "./ArrowLink.css";
+
+/**
+ * Create an unselectable space character.
+ */
+function UnselectableSpace() {
+  return <span className="UnselectableSpace" />;
+}
+
 /**
  * Create a link with an arrow next to it.
  */
 export default function ArrowLink({ to, children }) {
-  // TODO fix styling
   return (
-    <span>
+    <span className="ArrowLink">
       <Link to={to}>{children}</Link>
+      <UnselectableSpace />
       <BsArrowRight />
     </span>
   );
