a9aafdbf76ea8d71eca5b8724b6151fc39fc8ae5
Justin Yao Du <justinyaodu@gmail.com>
2021-07-04 23:39:42 -0700

    Add member info and more placeholder components

---
+++ src/components/Event.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+/**
+ * Display information about an event.
+ * @param props
+ * @param {string} props.title
+ * @param {string} props.description
+ * @param {string} props.place
+ * @param {Date} props.time
+ * @param {string} props.url
+ */
+export default function Event(/* { title, description, place, time, url } */) {
+  return <>Event placeholder</>;
+}
