00dafa275a917d1e322ab070d24bc887b2170c1f
Justin Yao Du <justinyaodu@gmail.com>
2021-07-05 14:27:34 -0700

    Add placeholder event data, components, and page (#61)

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,14 +1,29 @@
 import React from "react";
+import { DateTime } from "luxon";
+
+import ArrowLink from "./ArrowLink";
 
 /**
  * Display information about an event.
  * @param props
  * @param {string} props.title
  * @param {string} props.description
- * @param {string} props.place
- * @param {Date} props.time
+ * @param {string} props.location
+ * @param {luxon.DateTime} props.startTime
  * @param {string} props.url
  */
-export default function Event(/* { title, description, place, time, url } */) {
-  return <>Event placeholder</>;
+export default function Event({ title, description, location, startTime, url }) {
+  return (
+    <div>
+      <p>
+        <strong>{title}</strong>
+      </p>
+      <p>{description}</p>
+      <p>Where: {location}</p>
+      <p>When: {startTime.toLocaleString(DateTime.DATETIME_FULL)}</p>
+      <p>
+        <ArrowLink to={url}>Learn More</ArrowLink>
+      </p>
+    </div>
+  );
 }
