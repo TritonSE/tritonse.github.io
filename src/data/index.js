8e6f55219abf4f2d2aeff94787e4d452d9afe726
Justin Yao Du <justinyaodu@gmail.com>
2021-07-05 14:24:40 -0700

    Add placeholder event data, components, and page

--- src/data/index.js
+++ src/data/index.js
@@ -2,13 +2,21 @@
  * Import, process, and export data from JSON files.
  */
 
+import { DateTime } from "luxon";
+
 import allMembers from "./members.json";
+import allEvents from "./events.json";
 
 allMembers.forEach((member) => {
   member.socials = member.socials || {};
 });
-const shownMembers = allMembers.filter((member) => !member.hidden);
-const currentMembers = shownMembers.filter((member) => member.graduation_year === undefined);
-const alumni = shownMembers.filter((member) => member.graduation_year !== undefined);
 
-export { allMembers, currentMembers, alumni };
+const shownMembers = allMembers.filter((member) => !member.hidden);
+const currentMembers = shownMembers.filter((member) => member.graduationYear === undefined);
+const alumni = shownMembers.filter((member) => member.graduationYear !== undefined);
+
+allEvents.forEach((event) => {
+  event.startTime = DateTime.fromISO(event.startTime, { zone: "America/Los_Angeles" });
+});
+
+export { allMembers, currentMembers, alumni, allEvents };
