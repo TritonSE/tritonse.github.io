1bea9b35cc41354d1640688b738bf460cfabbc2d
Justin Yao Du <justinyaodu@gmail.com>
2021-07-08 12:30:30 -0700

    Fix lint hook on GitHub Desktop on Windows (#64)

---
+++ .husky/lint-config.sh
@@ -0,0 +1,24 @@
+# Change 1 to 0 to disable linting.
+enabled=1
+
+# Directories containing Node.js projects to be linted.
+node_dirs=(.)
+
+# Command used to run a lint check.
+check_command=(npm run lint-check)
+
+# Command used to autofix lint errors.
+fix_command=(npm run lint-fix)
+
+# Search for npm in these paths if it cannot be found.
+# Needed for GitHub Desktop on Windows, since $PATH is not set properly.
+npm_fallback_paths=('/c/Program Files/nodejs')
+
+# Escape sequences for formatted output.
+format_error="$(tput setaf 3 2> /dev/null)"
+format_warn="$(tput setaf 1 2> /dev/null)"
+format_clear="$(tput sgr0 2> /dev/null)"
+
+# The preceding commands may fail in environments where tput is not available.
+# Return 0 explicitly to indicate that the config was sourced successfully.
+return 0
