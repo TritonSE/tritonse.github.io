4c1feb2c8dbe29bcec35408ba87a6428c9d3fd63
NirmalAgnihotri <53363382+NirmalAgnihotri@users.noreply.github.com>
2021-07-07 19:14:20 -0700

    changed file name for navbar

--- src/components/NavbarT.js
+++ src/components/NavbarT.js
@@ -1,46 +1,24 @@
 import React from "react";
 import { Link } from "gatsby";
 import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 import { StaticImage } from "gatsby-plugin-image"
 
-export default function Navbar() {
+export default function NavbarT() {
   // Refer to: https://react-bootstrap.github.io/components/navbar/
   return (
-    <>
-      Navbar placeholder
       <Navbar expand="lg" bg="dark" variant="dark">
         <Navbar.Brand href="/"> <StaticImage src="../images/icons/tse-bulb.png" alt="Logo"/> </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar" />
         <Navbar.Collapse id="responsive-navbar">
-          <Navbar className="mr-auto">
+          <Nav className="mr-auto">
             <Navbar.Link href="/about">About Us</Navbar.Link>
             <Navbar.Link href="/approach">Our Approach</Navbar.Link>
             <Navbar.Link href="/projects">Projects</Navbar.Link>
             <Navbar.Link href="/students">For Students</Navbar.Link>
             <Navbar.Link href="/nonprofits">For Nonprofits</Navbar.Link>
-          </Navbar>
+          </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <ol>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About Us</Link>
-        </li>
-        <li>
-          <Link to="/approach">Our Approach</Link>
-        </li>
-        <li>
-          <Link to="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link to="/students">For Students</Link>
-        </li>
-        <li>
-          <Link to="/nonprofits">For Nonprofits</Link>
-        </li>
-      </ol>
-    </>
   );
 }
