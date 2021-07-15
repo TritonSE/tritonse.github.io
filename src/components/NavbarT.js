85bd32b06b14265d389831c8492faa6fcb7ef120
NirmalAgnihotri <53363382+NirmalAgnihotri@users.noreply.github.com>
2021-07-14 18:25:48 -0700

    Fixed navbar error

--- src/components/NavbarT.js
+++ src/components/NavbarT.js
@@ -1,24 +1,27 @@
 import React from "react";
 import { Link } from "gatsby";
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import { StaticImage } from "gatsby-plugin-image"
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { StaticImage } from "gatsby-plugin-image";
+import "./NavbarT.css";
 
 export default function NavbarT() {
   // Refer to: https://react-bootstrap.github.io/components/navbar/
   return (
-      <Navbar expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="/"> <StaticImage src="../images/icons/tse-bulb.png" alt="Logo"/> </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar" />
-        <Navbar.Collapse id="responsive-navbar">
-          <Nav className="mr-auto">
-            <Navbar.Link href="/about">About Us</Navbar.Link>
-            <Navbar.Link href="/approach">Our Approach</Navbar.Link>
-            <Navbar.Link href="/projects">Projects</Navbar.Link>
-            <Navbar.Link href="/students">For Students</Navbar.Link>
-            <Navbar.Link href="/nonprofits">For Nonprofits</Navbar.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+    <Navbar expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand href="/">
+        <StaticImage src="../images/icons/tse-bulb.png" alt="Logo" height={47} />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar" />
+      <Navbar.Collapse id="responsive-navbar">
+        <Nav className="mr-auto">
+          <Nav.Link href="/about">About Us</Nav.Link>
+          <Nav.Link href="/approach">Our Approach</Nav.Link>
+          <Nav.Link href="/projects">Projects</Nav.Link>
+          <Nav.Link href="/students">For Students</Nav.Link>
+          <Nav.Link href="/nonprofits">For Nonprofits</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
