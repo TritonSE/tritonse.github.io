6a456d32309e30ed74c1831303cf120fd449f9b4
Benson272 <55808666+Benson272@users.noreply.github.com>
2021-07-17 22:35:49 -0700

    Feature/benson272/1448387886 tabbed interface for profiles (#67)
    * tabbed interface for about us page
    
    * style about us profiles to be in grid format
    
    * added tab for client page as well as client logos to image folder
    
    * connected socials button to actual links
    
    * Fix replaceAll missing on older Node versions
    
    Co-authored-by: Justin Yao Du <justinyaodu@gmail.com>

--- src/data/index.js
+++ src/data/index.js
@@ -6,6 +6,7 @@ import { DateTime } from "luxon";
 
 import allMembers from "./members.json";
 import allEvents from "./events.json";
+import allClients from "./clients.json";
 
 allMembers.forEach((member) => {
   member.socials = member.socials || {};
@@ -14,9 +15,13 @@ allMembers.forEach((member) => {
 const shownMembers = allMembers.filter((member) => !member.hidden);
 const currentMembers = shownMembers.filter((member) => member.graduationYear === undefined);
 const alumni = shownMembers.filter((member) => member.graduationYear !== undefined);
+const clients = allClients;
+
+// TODO filter past and present clients
+// const currentClients = allClients.filter((client) => );
 
 allEvents.forEach((event) => {
   event.startTime = DateTime.fromISO(event.startTime, { zone: "America/Los_Angeles" });
 });
 
-export { allMembers, currentMembers, alumni, allEvents };
+export { allMembers, currentMembers, alumni, allEvents, clients };
