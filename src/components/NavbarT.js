66c7bed6f0a52e140912e31aed2597690b112e58
NirmalAgnihotri <53363382+NirmalAgnihotri@users.noreply.github.com>
2021-07-20 21:54:21 -0700

    Added Gatsby links to Navbar to stop refresh

--- src/components/NavbarT.js
+++ src/components/NavbarT.js
@@ -9,17 +9,39 @@ export default function NavbarT() {
   // Refer to: https://react-bootstrap.github.io/components/navbar/
   return (
     <Navbar expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="/">
-        <StaticImage src="../images/icons/tse-bulb.png" alt="Logo" height={47} />
+      <Navbar.Brand>
+        <Link to="/">
+          <StaticImage src="../images/icons/tse-bulb.png" alt="Logo" height={47} />
+        </Link>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar" />
       <Navbar.Collapse id="responsive-navbar">
-        <Nav className="mr-auto">
-          <Nav.Link href="/about">About Us</Nav.Link>
-          <Nav.Link href="/approach">Our Approach</Nav.Link>
-          <Nav.Link href="/projects">Projects</Nav.Link>
-          <Nav.Link href="/students">For Students</Nav.Link>
-          <Nav.Link href="/nonprofits">For Nonprofits</Nav.Link>
+        <Nav className="mr-auto" as="ul">
+          <Nav.Item as="li">
+            <Link to="/about" className="nav-link">
+              About us
+            </Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Link to="/approach" className="nav-link">
+              Our Approach
+            </Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Link to="/projects" className="nav-link">
+              Projects
+            </Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Link to="/students" className="nav-link">
+              For Students
+            </Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Link to="/nonprofits" className="nav-link">
+              For Nonprofits
+            </Link>
+          </Nav.Item>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
