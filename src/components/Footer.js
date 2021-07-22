cfb8da445f4242b3b13c590be44a1234f9352c6e
Dhruv Sood <dhruvsood27@gmail.com>
2021-07-21 18:51:32 -0700

    Linting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,41 +9,51 @@ export default function Footer() {
   // List of links: https://react-bootstrap.github.io/components/navs/#vertical
   // Icons: https://react-icons.github.io/react-icons
   // return <footer>Footer placeholder</footer>;
-  return(
+  return (
     // <footer className="container">
 
     <Container fluid className="container-fluid">
       <Row>
         {/* GENERAL */}
         <Col>
-        <h2>GENERAL</h2>
-        <Nav className="flex-column">
-          <Nav.Link bsPrefix="link">Home</Nav.Link>
-          <Nav.Link bsPrefix="link">About Us</Nav.Link>
-          <Nav.Link bsPrefix="link">Our Approach</Nav.Link>
-          <Nav.Link bsPrefix="link">Projects</Nav.Link>
-        </Nav>
+          <h2>GENERAL</h2>
+          <Nav className="flex-column">
+            <Nav.Link bsPrefix="link">Home</Nav.Link>
+            <Nav.Link bsPrefix="link">About Us</Nav.Link>
+            <Nav.Link bsPrefix="link">Our Approach</Nav.Link>
+            <Nav.Link bsPrefix="link">Projects</Nav.Link>
+          </Nav>
         </Col>
 
         {/* GET INVOLVED */}
         <Col xs={7}>
-        <h2>GET INVOLVED</h2>
-        <Nav className="flex-column">
-          <Nav.Link bsPrefix="link">Students</Nav.Link>
-          <Nav.Link bsPrefix="link">Non Profits</Nav.Link>
-        </Nav>
+          <h2>GET INVOLVED</h2>
+          <Nav className="flex-column">
+            <Nav.Link bsPrefix="link">Students</Nav.Link>
+            <Nav.Link bsPrefix="link">Non Profits</Nav.Link>
+          </Nav>
         </Col>
 
         {/* SOCIAL MEDIA */}
         <Col>
-        <h2></h2>
-        <Nav className="flex-column">
-          <Nav.Link bsPrefix="link" href="mailto:tse@ucsd.edu"><FaEnvelope/></Nav.Link>
-          <Nav.Link bsPrefix="link" href="https://facebook.com"><FaFacebook/></Nav.Link>
-          <Nav.Link bsPrefix="link" href="https://linkedin.com"><FaLinkedin/></Nav.Link>
-          <Nav.Link bsPrefix="link" href="https://github.com"><FaGithub/></Nav.Link>
-          <Nav.Link bsPrefix="link" href="https://instagram.com/ucsd_tse"><FaInstagram/></Nav.Link>
-        </Nav>
+          <h2 />
+          <Nav className="flex-column">
+            <Nav.Link bsPrefix="link" href="mailto:tse@ucsd.edu">
+              <FaEnvelope />
+            </Nav.Link>
+            <Nav.Link bsPrefix="link" href="https://facebook.com">
+              <FaFacebook />
+            </Nav.Link>
+            <Nav.Link bsPrefix="link" href="https://linkedin.com">
+              <FaLinkedin />
+            </Nav.Link>
+            <Nav.Link bsPrefix="link" href="https://github.com">
+              <FaGithub />
+            </Nav.Link>
+            <Nav.Link bsPrefix="link" href="https://instagram.com/ucsd_tse">
+              <FaInstagram />
+            </Nav.Link>
+          </Nav>
         </Col>
       </Row>
     </Container>
