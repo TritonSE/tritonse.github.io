b3d3f7f9ec0d66a18449bbb0ac0f6eadfbf335f7
Justin Yao Du <justinyaodu@gmail.com>
2021-07-24 15:12:25 -0700

    Remove commented-out code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,10 @@
 import React from "react";
 import { Container, Row, Col, Nav } from "react-bootstrap";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { FaEnvelope, FaFacebook, FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 import "./Footer.css";
 
 export default function Footer() {
-  // Column layout: https://react-bootstrap.github.io/layout/grid/
-  // List of links: https://react-bootstrap.github.io/components/navs/#vertical
-  // Icons: https://react-icons.github.io/react-icons
-  // return <footer>Footer placeholder</footer>;
   return (
-    // <footer className="container">
-
     <Container fluid className="container-fluid">
       <Row>
         {/* GENERAL */}
