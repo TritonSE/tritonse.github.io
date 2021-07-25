4d7f3e8aa164a5bec5331cb83ae7ad195dfaff65
Justin Yao Du <justinyaodu@gmail.com>
2021-07-24 22:24:56 -0700

    Add spacer column in footer to avoid wide links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -38,13 +38,12 @@ function FooterIcon({ icon, ...props }) {
 }
 
 export default function Footer() {
-  const iconSize = "2em";
   return (
     <Container className="Footer">
       <Row>
         <Col xs={9} sm={11}>
           <Row>
-            <Col className="mt-4" xs={12} sm={5} md={4} lg={3} xxl={2}>
+            <Col className="mt-4" xs={12} sm={5} md={4} lg={3} xl={3} xxl={2}>
               <FooterHeading>General</FooterHeading>
               <Nav className="flex-column">
                 <FooterItem to="/">Home</FooterItem>
@@ -53,13 +52,14 @@ export default function Footer() {
                 <FooterItem to="/projects">Projects</FooterItem>
               </Nav>
             </Col>
-            <Col className="mt-4" xs={12} sm={7} md={8} lg={9} xxl={10}>
+            <Col className="mt-4" xs={12} sm={7} md={5} lg={4} xl={4} xxl={2}>
               <FooterHeading>Get Involved</FooterHeading>
               <Nav className="flex-column">
                 <FooterItem to="/students">Students</FooterItem>
                 <FooterItem to="/nonprofits">Nonprofits</FooterItem>
               </Nav>
             </Col>
+            <Col xs={0} md={3} lg={5} xl={5} xxl={8} />
           </Row>
         </Col>
         <Col className="mt-4" xs={3} sm={1}>
