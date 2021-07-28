d0551d5656c218d815ec33370a3c491f602b2ec2
NirmalAgnihotri <53363382+NirmalAgnihotri@users.noreply.github.com>
2021-07-27 23:55:38 -0700

    centered cards on page

--- src/components/GetInvolved.js
+++ src/components/GetInvolved.js
@@ -2,6 +2,9 @@ import React from "react";
 import { StaticImage } from "gatsby-plugin-image";
 import "./GetInvolved.css";
 import InvolvedCard from "./InvolvedCard";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 
 export default function GetInvolved() {
   // Card: https://react-bootstrap.github.io/components/cards/
@@ -9,11 +12,25 @@ export default function GetInvolved() {
   // Use h2 for the title
   return (
     <>
-      <h3>Get Involved</h3>
-      <InvolvedCard imgPath="../images/icons/tse-bulb.png">
-        We recruit student developers and designers every academic year during the fall. Learn how
-        to join!
-      </InvolvedCard>
+      <h2>
+        <b>Get Involved</b>
+      </h2>
+      <Container style={{ display: "flex" }}>
+        <Row style={{ margin: "auto" }}>
+          <Col xs={10} md={6}>
+            <InvolvedCard imgPath="../images/icons/tse-bulb.png" title="Students">
+              We recruit student developers and designers every academic year during the fall. Learn
+              how to join!
+            </InvolvedCard>
+          </Col>
+          <Col xs={5} md={6}>
+            <InvolvedCard imgPath="../images/icons/tse-bulb.png" title="Non Profits">
+              We’re always looking for opportunities to help non profits operate with ease. Learn
+              how to partner with us!
+            </InvolvedCard>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
--- src/components/InvolvedCard.js
+++ src/components/InvolvedCard.js
@@ -3,17 +3,25 @@ import { StaticImage } from "gatsby-plugin-image";
 import "./GetInvolved.css";
 import Card from "react-bootstrap/Card";
 
-export default function InvolvedCard({ imgPath, children }) {
+export default function InvolvedCard({ imgPath, children, title }) {
   // Card: https://react-bootstrap.github.io/components/cards/
   // Don't worry about the close button for now
   // Use h2 for the title
   console.log(imgPath);
   return (
     <>
-      <Card className="custom-card">
+      <Card style={{ width: "18rem" }}>
+        <StaticImage
+          src="../images/icons/tse-bulb.png"
+          alt="InvolvedImage"
+          height={250}
+          width={250}
+          imgClassName="get-involved"
+        />
         <Card.Body>
+          <Card.Title>{title}</Card.Title>
           <StaticImage
-            src="../images/icons/tse-bulb.png"
+            src={imgPath}
             alt="InvolvedImage"
             height={250}
             width={250}
