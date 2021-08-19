cbceae408b8df0c41a44c217bf6b1b6ccd05f122
Benson272 <55808666+Benson272@users.noreply.github.com>
2021-08-18 21:40:47 -0700

    sorted members and alumnis (#80)
    * sorted members and alumnis
    
    * added VP Design role

--- src/components/Profiles.js
+++ src/components/Profiles.js
@@ -3,14 +3,17 @@ import React from "react";
 import Profile from "./Profile";
 import { clients, currentMembers, alumni } from "../data";
 import { nameToFilename } from "../util/strings";
+import { sortMembersByRole, categorizeMembersIntoYears } from "../util/members";
 
 function getProfilesFromCategory(category) {
   switch (category) {
     case "members":
+      sortMembersByRole(currentMembers);
       return currentMembers;
 
     case "alumni":
-      return alumni;
+      const result = categorizeMembersIntoYears(alumni);
+      return result;
 
     case "clients":
       return clients;
@@ -32,6 +35,8 @@ function getSocialsFromClients(profile) {
   return { website: profile.website };
 }
 
+getProfilesFromCategory("alumni");
+
 /**
  * Tabbed interface showing members, alumni, and clients.
  * @param props
@@ -43,19 +48,54 @@ export default function Profiles(props /* { members, clients } */) {
   return (
     <div className={`tab-content text-light row  ${props.display ? "d-flex" : "d-none"}`}>
       {
-        // creates a profile for each member/client
-        getProfilesFromCategory(props.name).map((profile) => (
-          <Profile
-            imagePaths={[`${category}/${nameToFilename(profile.name)}`, `${category}/anonymous`]}
-            title={profile.name}
-            subtitles={
-              category === "clients"
-                ? getSubtitlesFromClients(profile)
-                : getSubtitlesFromMembers(profile)
-            }
-            socials={category === "clients" ? getSocialsFromClients(profile) : profile.socials}
-          />
-        ))
+        // creates a profile for alumni that is categorized into graduation year
+        props.name === "alumni"
+          ? Object.keys(getProfilesFromCategory(props.name))
+              .reverse()
+              .map((yearRange) => (
+                <div>
+                  <h2 className="px-5 pt-5 mt-3">Class of {yearRange}</h2>
+                  <div className="row">
+                    {
+                      // loops through each alumni in the year range
+                      getProfilesFromCategory(props.name)[yearRange].map((profile) => (
+                        <Profile
+                          imagePaths={[
+                            `${category}/${nameToFilename(profile.name)}`,
+                            `${category}/anonymous`,
+                          ]}
+                          title={profile.name}
+                          subtitles={
+                            category === "clients"
+                              ? getSubtitlesFromClients(profile)
+                              : getSubtitlesFromMembers(profile)
+                          }
+                          socials={
+                            category === "clients"
+                              ? getSocialsFromClients(profile)
+                              : profile.socials
+                          }
+                        />
+                      ))
+                    }
+                  </div>
+                </div>
+              ))
+          : getProfilesFromCategory(props.name).map((profile) => (
+              <Profile
+                imagePaths={[
+                  `${category}/${nameToFilename(profile.name)}`,
+                  `${category}/anonymous`,
+                ]}
+                title={profile.name}
+                subtitles={
+                  category === "clients"
+                    ? getSubtitlesFromClients(profile)
+                    : getSubtitlesFromMembers(profile)
+                }
+                socials={category === "clients" ? getSocialsFromClients(profile) : profile.socials}
+              />
+            ))
       }
     </div>
   );
