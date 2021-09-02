4bf6d33e3e7a598fa12a4ab25dbdad93be75a1e8
Allison Bhavsar <albhavsa@ucsd.edu>
2021-09-01 18:24:29 -0700

    finished side by side component

--- src/components/SideBySide.js
+++ src/components/SideBySide.js
@@ -1,14 +1,17 @@
 import React from "react";
+import "./SideBySide.css";
 
 /**
  * Show content side-by-side on wide devices, or stacked on narrow ones.
  * @param {boolean} props.swap Whether to swap the two sides in horizontal mode.
  */
-export default function SideBySide({ /* swap = false, */ children }) {
+export default function SideBySide({ swap = false, children }) {
+  console.log(children);
   return (
-    <>
-      Side by side placeholder
-      {children}
-    </>
+    <div className={`side-by-side-wrapper ${swap ? "side-by-side-swapped" : "side-by-side"}`}>
+      {children.map((child) => (
+        <div className="side-by-side-element">{child}</div>
+      ))}
+    </div>
   );
 }
