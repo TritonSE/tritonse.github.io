18ebffdc15d901bdeafda8cc15ec11b2b5b1d0c0
Dhruv Sood <dhruvsood27@gmail.com>
2021-09-01 18:03:42 -0700

    Modified background image and video

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,6 @@
 import React from "react";
 import DynamicImage from "./DynamicImage";
-import { Jumbotron, Button } from "react-bootstrap";
+import { Jumbotron, Button, Container } from "react-bootstrap";
 
 import "./Hero.scss"
 
@@ -13,22 +13,44 @@ import "./Hero.scss"
  * @param {?{ text: string, url: string }[]} props.links Links to display as buttons.
  * @param {?boolean} props.centered Whether the text and buttons should be centered.
  */
-export default function Hero({ imagePath, title, description, links = [], centered = false }) {
+export default function Hero({ imagePath, videoId, title, description, links = [], centered }) {
   const images = require.context('../images/content', true);
   let imgsrc = images(`./${imagePath}.jpg`).default;
   const imgStyle = {
-    paddingTop: 100,
-    paddingBottom: 250,
-    backgroundColor: 'red',
+    // paddingTop: 150,
+    // paddingBottom: 180,
     backgroundSize: 'cover',
     backgroundImage: `url(${imgsrc})`,
     backgroundPosition: 'center'
   };
+  const vidStyle = {
+    // paddingTop: 150,
+    // paddingBottom: 180,
+    height: 50
+  }
   return (
-    <Jumbotron style={imgStyle}>
-      <div style={ centered ? { textAlign : 'center' } : {} } className="container">
-        <h1 className="display-4">{title}</h1>
-        <p className="lead">{description}</p>
+    // <Jumbotron className="hero" style={ (videoId.length == 0) ? { imgStyle } : { vidStyle }}>
+    // <Jumbotron className="hero" style={ (imagePath.length == 0) ? { vidStyle } : { imgStyle }}>
+    <Jumbotron className="hero" style={{imgStyle}}>
+      <Container style={ centered ? { textAlign : 'center' } : {} }>
+        {/* <div className="vidContainer">
+          <iframe className="ytVideo" 
+            frameborder="0" 
+            src={"https://www.youtube.com/embed/" + videoId + "?rel=0?version=3&autoplay=1&mute=1&controls=0&loop=1&playlist=" + videoId}
+            allow="autoplay"
+            style={ (videoId.length == 0) ? { width : 0, height : 0 } : { }}>
+          </iframe>
+        </div> */}
+        {/* <div className={ (videoId.length == 0) ? "imgContainer" : "vidContainer" }
+        style={ (videoId.length == 0) ? { backgroundImage : `url(${imgsrc})` } : { } }> */}
+          {/* <iframe className="ytVideo" 
+          frameborder="0" 
+          src={"https://www.youtube.com/embed/" + videoId + "?rel=0?version=3&autoplay=1&mute=1&controls=0&loop=1&playlist=" + videoId}
+          allow="autoplay"
+          style={ (videoId.length == 0) ? { width : 0 } : { }}></iframe> */}
+        {/* </div> */}
+        <h1 style={{marginTop: 150}}>{title}</h1>
+        <p>{description}</p>
         {links.length > 0 && (
           <ol className="buttonsList">
             {links.map(({ text, url }) => (
@@ -38,7 +60,7 @@ export default function Hero({ imagePath, title, description, links = [], center
             ))}
           </ol>
         )}
-      </div>
+      </Container>
     </Jumbotron>
   );
 }
