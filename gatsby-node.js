6eff9379c914a2a7ab1f047dc3a53e4381fdec3a
Allison Bhavsar <albhavsa@ucsd.edu>
2021-09-15 18:06:39 -0700

    Added more redirects for old project URLs

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,6 +10,50 @@ exports.createPages = async ({ actions }) => {
       fromPath: "/apply",
       toPath: "/students",
     },
+    {
+      fromPath: "/winners",
+      toPath: "/",
+    },
+    {
+      fromPath: "/project/uweast-ck-website",
+      toPath: "/projects/uweast-community-kitchen",
+    },
+    {
+      fromPath: "/project/dynamics-website",
+      toPath: "/projects",
+    },
+    {
+      fromPath: "/project/greenpoint-app",
+      toPath: "/projects",
+    },
+    {
+      fromPath: "/project/ihc-emr-app",
+      toPath: "/projects",
+    },
+    {
+      fromPath: "/project/ihc-wellness-app",
+      toPath: "/projects",
+    },
+    {
+      fromPath: "/project/kc-linking-app",
+      toPath: "/projects",
+    },
+    {
+      fromPath: "/project/kc-skilled-impact-app",
+      toPath: "/projects",
+    },
+    {
+      fromPath: "/project/cch-website",
+      toPath: "/projects",
+    },
+    {
+      fromPath: "/project/oic-website",
+      toPath: "/projects",
+    },
+    {
+      fromPath: "/project/ssa-website",
+      toPath: "/projects",
+    },
   ];
 
   redirects.forEach((redirect) => {
