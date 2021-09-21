3eb7fb894f47667863b0f75b70e70de6ef4ac244
NirmalAgnihotri <53363382+NirmalAgnihotri@users.noreply.github.com>
2021-09-21 16:50:26 -0700

    added scroll buttons to carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,51 +1,112 @@
-import React from "react";
-import Carousel from "react-bootstrap/Carousel";
+import React, { useRef } from "react";
+import CardGroup from "react-bootstrap/CardGroup";
 import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
-
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 import { BsArrowRight, BsArrowLeft } from "react-icons/bs";
 import ArrowLink from "./ArrowLink";
 import HoverCard from "./HoverCard";
 import "./Carousel.css";
 import { clients } from "../data";
+import PageContext from "./PageContext";
 
 export default function ProjectCarousel({ imgPath, children, title }) {
   // Card: https://react-bootstrap.github.io/components/cards/
   // Don't worry about the close button for now
   // Use h2 for the title
-  const renderedResult = [];
+  const listRef = useRef(null);
 
-  for (let i = 0; i < clients.length; i += 3) {
-    renderedResult.push(
-      <Carousel.Item key={i}>
-        <Container key={i}>
-          <Row key={i}>
-            {clients.slice(i, i + 3).map((client) => (
-              <Col xs="3" sm="4">
-                <HoverCard title={client.name} className="hover-card">
-                  A web and mobile application providing online children books, focused on language
-                  accessibility.
-                </HoverCard>
-              </Col>
-            ))}
-          </Row>
-        </Container>
-      </Carousel.Item>
-    );
+  const scrollList = (scrollDirection) => {
+    sideScroll(listRef.current, scrollDirection, 40, 200, 10);
+  };
+
+  function sideScroll(element, direction, speed, distance, step) {
+    let scrollAmount = 0;
+    var slideTimer = setInterval(function () {
+      if (direction == "left") {
+        element.scrollLeft -= step;
+      } else {
+        element.scrollLeft += step;
+      }
+      scrollAmount += step;
+      if (scrollAmount >= distance) {
+        window.clearInterval(slideTimer);
+      }
+    }, speed);
   }
+
   return (
-    <>
-      <h4>Latest Work</h4>
-      <Carousel
-        interval={null}
-        prevLabel=""
-        nextLabel=""
-        nextIcon={<BsArrowRight />}
-        prevIcon={<BsArrowLeft />}
-      >
-        {renderedResult}
-      </Carousel>
-    </>
+    <div className="project-carousel">
+      <h2>Latest Work</h2>
+      <Container style={{ overflow: "scroll" }} fluid ref={listRef}>
+        <div>
+          <PageContext.Consumer>
+            {({ allProjects }) => (
+              <div className="d-flex flex-row align-items-stretch">
+                {allProjects.map(({ frontmatter: { title, subtitle, thumbnail } }) => (
+                  <Col xs="12" sm="4" lg="4" key={title}>
+                    <HoverCard
+                      title={title}
+                      imgPath={[thumbnail || "icons/tse-bulb"]}
+                      altText="Project image"
+                      className="hover-card"
+                    >
+                      {subtitle}
+                    </HoverCard>
+                  </Col>
+                ))}
+                <Col xs="12" sm="4" lg="4">
+                  <HoverCard
+                    title={"FSD"}
+                    imgPath={["icons/tse-bulb"]}
+                    altText="Project image"
+                    className="hover-card"
+                  >
+                    TESTING
+                  </HoverCard>
+                </Col>
+                <Col xs="12" sm="4" lg="4">
+                  <HoverCard
+                    title={"FSD"}
+                    imgPath={["icons/tse-bulb"]}
+                    altText="Project image"
+                    className="hover-card"
+                  >
+                    TESTING
+                  </HoverCard>
+                </Col>
+                <Col xs="12" sm="4" lg="4">
+                  <HoverCard
+                    title={"FSD"}
+                    imgPath={["icons/tse-bulb"]}
+                    altText="Project image"
+                    className="hover-card"
+                  >
+                    TESTING
+                  </HoverCard>
+                </Col>
+              </div>
+            )}
+          </PageContext.Consumer>
+        </div>
+      </Container>
+      {window.innerWidth > 500 ? (
+        <ButtonGroup className="hidden-xs-down">
+          <Button
+            onClick={() => scrollList("left")}
+            className="carousel-control-prev hidden-xs-down"
+          >
+            <BsArrowLeft />
+          </Button>
+          <Button
+            onClick={() => scrollList("right")}
+            className="carousel-control-next hidden-xs-down"
+          >
+            <BsArrowRight />
+          </Button>
+        </ButtonGroup>
+      ) : null}
+    </div>
   );
 }
--- src/components/HoverCard.js
+++ src/components/HoverCard.js
@@ -7,7 +7,7 @@ import { clients } from "../data";
 import DynamicImage from "./DynamicImage";
 import { nameToFilename } from "../util/strings";
 
-export default function HoverCard({ imgPath, children, title, isGetInvolved }) {
+export default function HoverCard({ imgPath, children, title, isGetInvolved, altText }) {
   // Card: https://react-bootstrap.github.io/components/cards/
   // Don't worry about the close button for now
   // Use h2 for the title
@@ -15,20 +15,10 @@ export default function HoverCard({ imgPath, children, title, isGetInvolved }) {
   return (
     <>
       <Card style={{ width: "18rem" }}>
-        <DynamicImage
-          paths={[`clients/${nameToFilename(title)}`, "clients/anonymous"]}
-          height={250}
-          width={250}
-          alt="projectimage"
-        />
+        <div className="image-control">
+          <DynamicImage paths={imgPath} height={250} width={250} alt={altText} />
+        </div>
 
-        {/* <StaticImage
-          src="../images/icons/tse-bulb.png"
-          alt="InvolvedImage"
-          height={250}
-          width={250}
-          imgClassName={isGetInvolved ? "get-involved" : ""}
-        /> */}
         <Card.Body>
           <Card.Title style={{ color: "black" }}>{title}</Card.Title>
           <Card.Text style={{ color: "black" }}>{children}</Card.Text>
