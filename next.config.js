f4ec01ab20237ce84cf12374b8aedcdc06269b08
Justin Yao Du <justinyaodu@gmail.com>
2022-01-31 15:05:23 -0800

    Set up static site generation

--- next.config.js
+++ next.config.js
@@ -6,11 +6,19 @@ const withMDX = require("@next/mdx")({
     rehypePlugins: [],
   },
 });
+const withOptimizedImages = require("next-optimized-images");
 
-module.exports = withMDX({
-  reactStrictMode: true,
-  pageExtensions: ["tsx", "mdx"],
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-});
+module.exports = withOptimizedImages(
+  withMDX({
+    reactStrictMode: true,
+    pageExtensions: ["tsx", "mdx"],
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    images: {
+      loader: "akamai",
+      path: "/",
+      disableStaticImages: true,
+    },
+  })
+);
