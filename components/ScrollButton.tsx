71e2d024b72009c5c99b84887ca24c7dfd5b8e3a
jenniferchen11 <jennifer.chen2002@gmail.com>
2023-01-19 17:13:59 -0800

    Scroll button functionality

---
+++ components/ScrollButton.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+
+import CustomImage from "./CustomImage";
+
+export default function Navbar() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300) {
+      setIsVisible(true);
+    } else if (scrolled <= 300) {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+      /* you can also use 'auto' behaviour
+             in place of 'smooth' */
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisible);
+  });
+
+  return (
+    <a
+      style={{
+        display: isVisible ? "inline" : "none",
+        position: "fixed",
+        bottom: "3px",
+        right: "30px",
+      }}
+    >
+      <CustomImage
+        onClick={scrollToTop}
+        imageKey="icons/scroll-to-top"
+        alt="Logo"
+        width={50}
+        height={50}
+      />
+    </a>
+  );
+}
