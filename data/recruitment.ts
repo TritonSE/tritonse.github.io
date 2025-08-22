c489c9aab3ed0a0be58935fd8ac3af4913ba69aa
Benjamin Johnson <ben332004@gmail.com>
2025-08-21 18:56:34 -0700

    Switch from president to TSE email + fix rendering bug (#159)
    * Switch from president to TSE email + fix rendering bug
    
    * fix linter errors
    
    * Separate GH action for testing links
    
    * fix formatting

--- data/recruitment.ts
+++ data/recruitment.ts
@@ -2,7 +2,6 @@ const recruitment = {
   acceptingApplications: false,
   applicationUrl: "https://tritonse.github.io/TSE-Application-Form/",
   deadline: "Sunday, October 13th at 11:59PM PDT",
-  presidentEmail: "v6liu@ucsd.edu",
 } as const;
 
 export default recruitment;
