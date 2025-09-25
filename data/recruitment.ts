b3d0ffca45afd5c85a64785e6940d5fbe1fb089f
Benjamin Johnson <ben332004@gmail.com>
2025-09-25 08:35:29 -0700

    Open application form

--- data/recruitment.ts
+++ data/recruitment.ts
@@ -1,5 +1,5 @@
 const recruitment = {
-  acceptingApplications: false,
+  acceptingApplications: true,
   applicationUrl: "https://fulcrum-5vpct.ondigitalocean.app/apply",
   deadline: "Sunday, October 12th at 11:59PM PDT",
 } as const;
