4d8de5220aa102d57fe0cea387ae0d9067401abf
mraysu <mike.ray.sullivan@gmail.com>
2026-09-23 23:45:35 -0700

    accepting applications 2026

--- data/recruitment.ts
+++ data/recruitment.ts
@@ -1,5 +1,5 @@
 const recruitment = {
-  acceptingApplications: false,
+  acceptingApplications: true,
   applicationUrl:
     "https://tse-fulcrum-web-dfg7d9gccygmepgp.westcentralus-01.azurewebsites.net/apply",
   deadline: "Sunday, October 11th at 11:59PM PDT",
